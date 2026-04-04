bfedb0d1c19c9c04352df31efd89ac39f262823c
yahiro <yahiro07@users.noreply.github.com>
2026-04-05 01:20:38 +0900

    define parameters

--- proto-kit-auv3/xcode-swiftpm/MyAudioPluginPackage/Sources/Auv3Framework/PluginApp/Parameters.swift
+++ proto-kit-auv3/xcode-swiftpm/MyAudioPluginPackage/Sources/Auv3Framework/PluginApp/Parameters.swift
@@ -1,45 +1,52 @@
 import AudioToolbox
-import Auv3DspCore
 import Foundation
 
 let Project1ExtensionParameterSpecs = ParameterTreeSpec {
   ParameterGroupSpec(identifier: "global", name: "Global") {
     ParameterSpec(
-      address: .gain,
-      identifier: "gain",
-      name: "Output Gain",
-      units: .linearGain,
-      valueRange: 0.0...1.0,
-      defaultValue: 0.25
+      address: 0,
+      identifier: "parametersVersion",
+      name: "Parameters Version",
+      units: .generic,
+      valueRange: 0...999999,
+      defaultValue: 1,
+      flags: [AudioUnitParameterOptions.flag_IsGlobalMeta]
     )
-  }
-}
-
-extension ParameterSpec {
-  init(
-    address: Project1ExtensionParameterAddress,
-    identifier: String,
-    name: String,
-    units: AudioUnitParameterUnit,
-    valueRange: ClosedRange<AUValue>,
-    defaultValue: AUValue,
-    unitName: String? = nil,
-    flags: AudioUnitParameterOptions = [
-      AudioUnitParameterOptions.flag_IsWritable, AudioUnitParameterOptions.flag_IsReadable,
-    ],
-    valueStrings: [String]? = nil,
-    dependentParameters: [NSNumber]? = nil
-  ) {
-    self.init(
-      address: address.rawValue,
-      identifier: identifier,
-      name: name,
-      units: units,
-      valueRange: valueRange,
-      defaultValue: defaultValue,
-      unitName: unitName,
-      flags: flags,
-      valueStrings: valueStrings,
-      dependentParameters: dependentParameters)
+    ParameterSpec(
+      address: 1,
+      identifier: "oscEnabled",
+      name: "Osc Enabled",
+      units: .boolean,
+      valueRange: 0...1,
+      defaultValue: 1,
+      flags: []
+    )
+    ParameterSpec(
+      address: 2,
+      identifier: "oscWave",
+      name: "Osc Wave",
+      units: .generic,
+      valueRange: 0...3,
+      defaultValue: 0,
+      flags: []
+    )
+    ParameterSpec(
+      address: 3,
+      identifier: "oscPitch",
+      name: "Osc Pitch",
+      units: .generic,
+      valueRange: 0...1,
+      defaultValue: 0.5,
+      flags: []
+    )
+    ParameterSpec(
+      address: 4,
+      identifier: "oscVolume",
+      name: "Osc Volume",
+      units: .generic,
+      valueRange: 0...1,
+      defaultValue: 0.5,
+      flags: []
+    )
   }
 }
