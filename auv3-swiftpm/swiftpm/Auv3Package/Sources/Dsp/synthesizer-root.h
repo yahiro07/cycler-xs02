0f83fee52c309e0252a9c9e360000d5efe8626aa
yahiro <yahiro07@users.noreply.github.com>
2026-04-11 15:05:54 +0900

    clear existing implementation

--- auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/synthesizer-root.h
+++ auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/synthesizer-root.h
@@ -1,170 +1,22 @@
 #pragma once
 #include "./base/api.h"
-#include "./base/parameter-id.h"
-#include <atomic>
-#include <cmath>
-#include <cstdio>
-
-enum class OscWave : int {
-  Saw = 0,
-  Rect,
-  Tri,
-  Sine,
-  // Noise
-};
-
-enum class FilterType : int { LPF = 0, BPF, HPF };
-
-enum class LfoWave : int { Sine = 0, Tri, Rect, Saw };
-
-enum class LfoDestination : int {
-  None = 0,
-  Osc1Pitch,
-  Osc1PWMix,
-  Osc1Volume,
-  Osc2Pitch,
-  Osc2PWMix,
-  Osc2Volume,
-  FilterCutoff,
-  AmpVolume,
-};
-
-inline float getFormulaicOscWave(OscWave wave, float phase) {
-  if (wave == OscWave::Sine) {
-    return std::sin(phase * 2.0f * M_PI);
-  } else if (wave == OscWave::Saw) {
-    return (2.0f * phase - 1.0f);
-  } else if (wave == OscWave::Rect) {
-    return (phase < 0.5f ? 1.0f : -1.0f);
-  } else if (wave == OscWave::Tri) {
-    return (phase < 0.5f ? (4.0f * phase - 1.0f) : (-4.0f * phase + 3.0f));
-  }
-  return 0.0f;
-}
-
-template <typename EnumType> inline EnumType paramToEnum(float value) {
-  return static_cast<EnumType>(static_cast<int>(std::lround(value)));
-}
-
-inline bool paramToBool(float value) { return (value >= 0.5f); }
-
-inline float MIDINoteToFrequency(int note) {
-  return 440.0f * std::pow(2, ((note - 69) / 12.0));
-}
-
-typedef struct {
-  int parametersVersion;
-  bool oscOn;
-  OscWave osc1Wave;
-  float osc1Octave;
-  float osc1Volume;
-} SynthesisParameters;
-
-typedef ParameterId PK;
-
-inline void applySynthesisParameter(SynthesisParameters &sp, uint64_t id,
-                                    float value) {
-  // printf("applySynthesisParameter %llu %f\n", id, value);
-  if (id == PK::parametersVersion) {
-    sp.parametersVersion = static_cast<int>(std::lround(value));
-  } else if (id == PK::oscOn) {
-    sp.oscOn = paramToBool(value);
-  } else if (id == PK::oscWave) {
-    sp.osc1Wave = paramToEnum<OscWave>(value);
-  } else if (id == PK::oscOctave) {
-    sp.osc1Octave = value;
-  } else if (id == PK::synthVolume) {
-    sp.osc1Volume = value;
-  }
-}
-
-struct SynthesizerStateBus {
-  SynthesisParameters synthesisParameters;
-  float sampleRate = 0.f;
-  int noteNumber = 60;
-  bool gateOn = false;
-  float bpm = 120.0;
-  bool playState = false;
-  float gateOnUptimeSec = 0.f;
-  std::atomic<bool> needRandomize = false;
-};
-
-static const int noteNumber_globalPlayState = 200;
 
 class SynthesizerRoot : public IDspCore {
-private:
-  float mPhase = 0.0;
-  SynthesizerStateBus bus;
 
 public:
-  void prepareProcessing(double sampleRate, uint32_t maxFrameLength) override {
-    // printf("⭐️sr 2144\n");
-    bus.sampleRate = static_cast<float>(sampleRate);
-  }
+  void prepareProcessing(double sampleRate, uint32_t maxFrameLength) override {}
 
-  void setParameter(uint64_t id, double value) override {
-    if (id == PK::internalBpm) {
-      bus.bpm = static_cast<float>(value);
-    } else {
-      applySynthesisParameter(bus.synthesisParameters, id, value);
-    }
-  }
+  void setParameter(uint64_t id, double value) override {}
 
-  void noteOn(int noteNumber, double velocity) override {
-    bus.noteNumber = noteNumber;
-    bus.gateOn = true;
-    bus.gateOnUptimeSec = 0.f;
-  }
-  void noteOff(int noteNumber) override {
-    if (noteNumber == bus.noteNumber) {
-      bus.gateOn = false;
-    }
-  }
+  void noteOn(int noteNumber, double velocity) override {}
+  void noteOff(int noteNumber) override {}
 
   void processAudio(float *leftBuffer, float *rightBuffer,
-                    uint32_t frames) override {
-    if (bus.sampleRate == 0.f)
-      return;
-    auto sp = bus.synthesisParameters;
-    auto ni = bus.noteNumber + ((sp.osc1Octave * 4) - 2) * 12;
-    auto freq = MIDINoteToFrequency(ni);
-    auto delta = freq / bus.sampleRate;
-    auto prWave = sp.osc1Wave;
-    auto gain = sp.oscOn && bus.gateOn ? sp.osc1Volume : 0.0f;
+                    uint32_t frames) override {}
 
-    for (int i = 0; i < frames; ++i) {
-      mPhase += delta;
-      if (mPhase >= 1.0f)
-        mPhase -= 1.0f;
-      auto y = getFormulaicOscWave(prWave, mPhase) * gain;
-      leftBuffer[i] = y;
-      rightBuffer[i] = y;
-    }
-    auto elapsed = (float)frames / bus.sampleRate;
-    auto currUptime = bus.gateOnUptimeSec;
-    auto nextUptime = bus.gateOnUptimeSec + elapsed;
-    if (bus.gateOn) {
-      auto currSec = static_cast<int>(currUptime / 1.0);
-      auto nextSec = static_cast<int>(nextUptime / 1.0);
-      if (currSec != nextSec) {
-        bus.needRandomize.store(true, std::memory_order_release);
-      }
-    }
-    bus.gateOnUptimeSec = nextUptime;
-  }
+  void applyCommand(uint64_t id, double value) override {}
 
-  void applyCommand(uint64_t id, double value) override {
-    if (id == CommandId::setPlayState) {
-      bus.playState = paramToBool(value);
-    }
-  }
-
-  bool extraLogic_pullRandomizeRequestFlag() override {
-    return bus.needRandomize.exchange(false, std::memory_order_acq_rel);
-  }
+  bool extraLogic_pullRandomizeRequestFlag() override { return false; }
   void extraLogic_randomizeParameters(
-      std::map<uint64_t, double> &parameters) override {
-    parameters[PK::oscWave] = 1;
-    parameters[PK::oscOctave] = rand() / (double)RAND_MAX;
-  }
+      std::map<uint64_t, double> &parameters) override {}
 };
