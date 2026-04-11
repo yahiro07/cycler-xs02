4d8198d1693e46bd3d1dbe6e71778568f361bf7c
yahiro <yahiro07@users.noreply.github.com>
2026-04-11 16:10:24 +0900

    update types

--- auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/base/api.h
+++ auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/base/api.h
@@ -8,20 +8,19 @@ class IDspCore {
 public:
   virtual ~IDspCore() = default;
   // main thread
-  virtual void prepareProcessing(double sampleRate, uint32_t maxFrames) = 0;
+  virtual void prepareProcessing(float sampleRate, int maxFrames) = 0;
 
   // audio thread
-  virtual void setParameter(uint64_t id, double value) = 0;
-  virtual void noteOn(int noteNumber, double velocity) = 0;
+  virtual void setParameter(uint64_t id, float value) = 0;
+  virtual void noteOn(int noteNumber, float velocity) = 0;
   virtual void noteOff(int noteNumber) = 0;
-  virtual void processAudio(float *bufferL, float *bufferR,
-                            uint32_t frames) = 0;
-  virtual void applyCommand(uint64_t id, double value) = 0;
+  virtual void processAudio(float *bufferL, float *bufferR, int frames) = 0;
+  virtual void applyCommand(uint64_t id, float value) = 0;
 
   // main thread
   virtual bool extraLogic_pullRandomizeRequestFlag() = 0;
   virtual void
-  extraLogic_randomizeParameters(std::map<uint64_t, double> &parameters) = 0;
+  extraLogic_randomizeParameters(std::map<uint64_t, float> &parameters) = 0;
 };
 
 } // namespace dsp
--- auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/synthesizer-root.h
+++ auv3-swiftpm/swiftpm/Auv3Package/Sources/Dsp/synthesizer-root.h
@@ -6,20 +6,20 @@ namespace dsp {
 class SynthesizerRoot : public IDspCore {
 
 public:
-  void prepareProcessing(double sampleRate, uint32_t maxFrameLength) override {}
+  void prepareProcessing(float sampleRate, int maxFrameLength) override {}
 
-  void setParameter(uint64_t id, double value) override {}
+  void setParameter(uint64_t id, float value) override {}
 
-  void noteOn(int noteNumber, double velocity) override {}
+  void noteOn(int noteNumber, float velocity) override {}
   void noteOff(int noteNumber) override {}
 
   void processAudio(float *leftBuffer, float *rightBuffer,
-                    uint32_t frames) override {}
+                    int frames) override {}
 
-  void applyCommand(uint64_t id, double value) override {}
+  void applyCommand(uint64_t id, float value) override {}
 
   bool extraLogic_pullRandomizeRequestFlag() override { return false; }
   void extraLogic_randomizeParameters(
-      std::map<uint64_t, double> &parameters) override {}
+      std::map<uint64_t, float> &parameters) override {}
 };
 } // namespace dsp
