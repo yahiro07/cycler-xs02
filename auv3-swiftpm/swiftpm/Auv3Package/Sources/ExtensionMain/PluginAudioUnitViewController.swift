d6a521d4d224368253ef87db2c49124f3240c12c
yahiro <yahiro07@users.noreply.github.com>
2026-04-13 18:08:27 +0900

    remove unused code

--- auv3-swiftpm/swiftpm/Auv3Package/Sources/ExtensionMain/PluginAudioUnitViewController.swift
+++ auv3-swiftpm/swiftpm/Auv3Package/Sources/ExtensionMain/PluginAudioUnitViewController.swift
@@ -71,17 +71,13 @@ open class PluginAudioUnitViewController: AUViewController, AUAudioUnitFactory {
           self.configureSwiftUIView(audioUnit: audioUnit)
         }
       }
+
       // self.observation = audioUnit.observe(\.allParameterValues, options: [.new]) {
       //   object, change in
       //   guard let tree = audioUnit.parameterTree else { return }
 
       //   // This insures the Audio Unit gets initial values from the host.
       //   for param in tree.allParameters { param.value = param.value }
-      // }
-
-      // guard audioUnit.parameterTree != nil else {
-      //   logger.error("Unable to access AU ParameterTree")
-      //   return audioUnit
       // }
 
       return audioUnit
