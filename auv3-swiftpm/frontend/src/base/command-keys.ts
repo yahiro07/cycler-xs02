c5a2a86f9660c37c49881340df9e2262930b6b91
yahiro <yahiro07@users.noreply.github.com>
2026-04-14 17:15:52 +0900

    disable preset manager

--- auv3-swiftpm/frontend/src/base/command-keys.ts
+++ auv3-swiftpm/frontend/src/base/command-keys.ts
@@ -1,19 +1,3 @@
-// export enum ControlCommand {
-//   kcSetGroovePlaying = 0, // UI-->App-->DSP
-//   kcNoteOn, // App-->UI, APP-->DSP
-//   kcNoteOff, // App-->UI, APP-->DSP
-//   kcSetBpmToDsp, // App-->DSP
-//   kcSetParamVer, // App-->UI, App-->DSP
-//   kcSetIsHostedOnDAW, // App-->UI
-//   kcSendSP_hostBpm, // App-->UI
-//   kcSendSP_standaloneBpm, // App<-->UI, persist
-//   kcSendSP_autoRandomizeOnLoop, // App<-->UI, persist
-//   kcSendSP_randomizeLevel, // App<-->UI, persist
-//   kcSendSP_touchPointerVisible, // App<-->UI, persist
-//   kcResetParameters, // UI-->App
-//   kcRandomizeParameters, // UI-->App
-// }
-
 export type CommandKeyFromApp = "setStandaloneFlag";
 
 export type CommandKeyFromUi =
