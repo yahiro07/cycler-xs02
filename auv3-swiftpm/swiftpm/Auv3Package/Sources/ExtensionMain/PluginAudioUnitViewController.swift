63350fbd17b12e844bfc7c5c7b8b5a3717bde011
yahiro <yahiro07@users.noreply.github.com>
2026-04-14 17:44:46 +0900

    update bundle identifier

--- auv3-swiftpm/swiftpm/Auv3Package/Sources/ExtensionMain/PluginAudioUnitViewController.swift
+++ auv3-swiftpm/swiftpm/Auv3Package/Sources/ExtensionMain/PluginAudioUnitViewController.swift
@@ -52,7 +52,7 @@ open class PluginAudioUnitViewController: AUViewController, AUAudioUnitFactory {
   {
     logger.trace("createAudioUnit 1329")
     showEntryInfo(componentDescription)
-    SharedContainer.setAppGroupId("group.com.example.sonic.proto-kit-auv3.CyclerXS02")
+    SharedContainer.setAppGroupId("group.net.miqsel.cycler.xs02")
 
     return try DispatchQueue.main.sync {
 
